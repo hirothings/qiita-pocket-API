63476d3764035fa9ac7e619ff3eada9003843ce2
hirothings <hiroshings.job1987@gmail.com>
2017-10-05 21:59:17 +0900

    Use SSL

--- update-posts.rb
+++ update-posts.rb
@@ -3,8 +3,6 @@ require 'uri'
 
 url = URI.parse('https://qiita-pocket-api.herokuapp.com/articles')
 req = Net::HTTP::Post.new(url.path)
-res = Net::HTTP.new(url.host, url.port).start do |http|
-  http.request(req)
-end
-
-puts res.body
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+res = http.request(req)
